c3ff0993d830e47a0d4b3f4c70c4ee2dd640bae5
Rachayita Giri <rachayitagiri@gmail.com>
2024-11-20 18:24:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from Preference import Preference
 from voting import *
 
+def test_dictatorship():
+    pass
+
+def test_scoring_rule():
+    pass
+
+def test_plurality():
+    pass
+
+def test_veto():
+    pass
+
+def test_borda():
+    pass
+
+def test_STV():
+    pass
+
+if __name__ == "__main__":
+    # Call test methods here
