60d7f88ea0e607da30b617cf87b60de71ade914b
Rachayita Giri <rachayitagiri@gmail.com>
2024-11-20 18:47:11 +0530

    update main.py to test voting.dictatorship()

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from Preference import Preference
 from voting import *
 
-def test_dictatorship():
-    pass
+def test_dictatorship_success(pref_obj):
+    print(f"Testing dictatorship() success...")
+    for voter in pref_obj.voters():
+        winner = dictatorship(pref_obj, voter)
+        print(f"Voter: {voter} | Winner: {winner}")
+    return
+
+def test_dictatorship_failure(pref_obj):
+    print(f"Testing dictatorship() failure...")
+    try:
+        dictatorship(pref_obj, 4)
+    except ValueError as e:
+        print(e)
+    return
 
 def test_scoring_rule():
     pass
@@ -20,4 +32,10 @@ def test_STV():
     pass
 
 if __name__ == "__main__":
-    # Call test methods here
+    my_pref_data = Preference({
+        1: [3, 1, 2],
+        2: [1, 3, 2],
+        3: [2, 1, 3]
+    })
+    test_dictatorship_success(my_pref_data)
+    test_dictatorship_failure(my_pref_data)
